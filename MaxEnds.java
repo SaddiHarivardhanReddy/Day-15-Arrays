7fabac6a361224075bcad67d72171f1c879aff29
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-30 22:29:03 +0530

    Set All to Maximum of Ends

---
+++ MaxEnds.java
@@ -0,0 +1,27 @@
+package com.codegnan.arrays;
+
+import java.util.Scanner;
+
+public class MaxEnds {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int max=arr[0];
+		for(int i=0;i<n;i++) {
+			if(arr[i]>max) {
+				max=arr[i];
+			}
+		}
+		for(int i=0;i<n;i++) {
+			arr[i]=max;
+		System.out.print(arr[i]+" ");
+		}
+		sc.close();
+	}
+
+}
