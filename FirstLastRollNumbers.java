41e54538cd01f7ca194a6e5fa3fa924ecb77dfe9
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2025-10-30 22:28:04 +0530

    First and Last Roll Numbers

---
+++ FirstLastRollNumbers.java
@@ -0,0 +1,18 @@
+package com.codegnan.arrays;
+
+import java.util.Scanner;
+
+public class FirstLastRollNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(arr[0]+" "+arr[n-1]);
+		sc.close();
+	}
+
+}
